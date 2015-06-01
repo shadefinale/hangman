99f0da600b28049a1c22b4a4b2417e179580b6d3
shadefinale <shadefinale@gmail.com>
2015-06-01 08:52:38 -0500

    Added more tests and removed unnecessary comments.

--- spec/tests.rb
+++ spec/tests.rb
@@ -14,51 +14,47 @@ describe "Main" do
     ng.save
   end
 
-  # After each test, we reset the guesses to 5 and the guessed letters to "". (The defaults)
+  # After each test, we reset the dummy game or regenerate it if it's been deleted.
   after(:each) do
-    Game.get(1).update(:guesses => 5)
-    Game.get(1).update(:letters => "")
+    if Game.get(1)
+      Game.get(1).update(:guesses => 5)
+      Game.get(1).update(:letters => "")
+    else
+      ng = Game.new
+      ng.attributes = {:id => 1, :word => 'test'}
+      ng.save
+    end
   end
 
   describe "Routes" do
     it "should redirect / to /game/new" do
       get '/'
       follow_redirect!
-      expect(last_response.body).to include("Select a difficulty and let's begin!")
+      expect(last_request.url).to include("/game/new")
     end
 
     it "should show new game page" do
       get '/game/new'
-      expect(last_response.body).to include("Select a difficulty and let's begin!")
+      expect(last_request.url).to include("/game/new")
     end
 
     it "should start a new game if post to /game/new" do
       post '/game/new', {:difficulty => 4}
       follow_redirect!
       expect(last_request.url).to include("/game/2")
-      expect(last_response.body).to include("Guess a letter!")
     end
 
-    # Make sure that we can't access a game that doesn't exist
     it "should redirect to /game/new/ if game does not exist" do
       get '/game/920482'
       follow_redirect!
       expect(last_request.url).to include("/game/new")
     end
 
-    # Make sure that we even get a game returned.
-    it "should have a game 1 with the word test in it" do
-      get '/game/1'
-      expect(last_response.body).to include("Guess a letter!")
-    end
-
-    # Make sure that our dummy game has four underscores due to 'test' being four letters.
-    it "should have game 1 show '_ _ _ _'" do
+    it "should have game 1 show correct amount of underscores" do
       get 'game/1'
       expect(last_response.body).to include("_ _ _ _")
     end
 
-    # Correct guesses should fill in the proper blanks.
     it "should accept T as a correct guess to game 1" do
       get '/game/1'
       post '/game/1', {:guess => "T"}
@@ -66,7 +62,6 @@ describe "Main" do
       expect(last_response.body).to include("T _ _ T")
     end
 
-    # A wrong guess should show up in the wrong letters category, and also subtract a guess.
     it "should show that W is an incorrect guess to game 1." do
       get '/game/1'
       post '/game/1', {:guess => "W"}
@@ -74,6 +69,30 @@ describe "Main" do
       expect(last_response.body).to include("_ _ _ _")
       expect(last_response.body).to include("Wrong Letters: W")
       expect(Game.get(1).guesses).to eq(4)
+    end
+
+    it "should game over when too many wrong inputs guessed" do
+      get '/game/1'
+      post '/game/1', {:guess => "A"}
+      follow_redirect!
+      post '/game/1', {:guess => "B"}
+      follow_redirect!
+      post '/game/1', {:guess => "C"}
+      follow_redirect!
+      post '/game/1', {:guess => "D"}
+      follow_redirect!
+      post '/game/1', {:guess => "F"}
+      expect(last_response.body).to include("out of guesses")
+    end
+
+    it "should show a congratulation screen when guessed correctly" do
+      get '/game/1'
+      post '/game/1', {:guess => "T"}
+      follow_redirect!
+      post '/game/1', {:guess => "E"}
+      follow_redirect!
+      post '/game/1', {:guess => "S"}
+      expect(last_response.body).to include("did it!")
     end
   end
 end
